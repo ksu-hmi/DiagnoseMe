7a9d49b91250fe376fafb72323ced0ade146125b
denisethomas064 <137345050+denisethomas064@users.noreply.github.com>
2023-11-14 22:26:40 -0500

    Updates: added interactive choices for user

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import requests
 from bs4 import BeautifulSoup
 
@@ -11,29 +11,50 @@ def scrape_website(url):
     response = requests.get(url)
     if response.status_code == 200:
         soup = BeautifulSoup(response.text, 'html.parser')
-        title = soup.title.string
-        links = soup.find_all('a')
-        return {'title': title, 'links': [(link.text, link.get('href')) for link in links]}
+        # Assuming diseases are listed in hyperlinks under the "Directory: Guidance on prevention and control" section
+        disease_links = soup.select('div#content div.main-content a')
+        diseases = [link.get_text() for link in disease_links]
+        return {'diseases': diseases}
     else:
         return None
 
+# Sample list of symptoms
+sample_symptoms = [
+    'fever',
+    'cough',
+    'headache',
+    # Add more symptoms as needed
+]
+
+# Base URL for scraping
+base_url = "https://www.ecdc.europa.eu/en/all-topics"
+
 # Routes
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    # Define the URLs to scrape
-    urls = [
-        "https://www.ecdc.europa.eu/en/all-topics",
-        "https://www.nhsinform.scot/illnesses-and-conditions/a-to-z/",
-    ]
+    if request.method == 'POST':
+        selected_symptoms = request.form.getlist('symptoms')
+        # Define the URLs to scrape
+        urls = [base_url]
 
-    # Scrape data from the URLs
-    scraped_data = []
-    for url in urls:
-        data = scrape_website(url)
-        if data:
-            scraped_data.append(data)
+        # Scrape data from the URLs
+        scraped_data = []
+        for url in urls:
+            data = scrape_website(url)
+            if data:
+                scraped_data.append(data)
 
-    return render_template('index.html', data=scraped_data)
+        # Filter diseases based on selected symptoms (dummy logic)
+        filtered_diseases = []
+        for disease in scraped_data[0].get('diseases', []):
+            # Check if all selected symptoms are present in the disease name
+            if all(symptom.lower() in disease.lower() for symptom in selected_symptoms):
+                filtered_diseases.append(disease)
+
+        return render_template('index.html', data=filtered_diseases, symptoms=sample_symptoms)
+
+    return render_template('index.html', data=None, symptoms=sample_symptoms)
 
 if __name__ == '__main__':
     app.run(debug=True)
+
